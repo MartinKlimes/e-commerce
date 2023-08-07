d02bc10b659a3647e4892b5f291acee149d62803
MatePC <xklime44@vutbr.cz>
2023-08-07 21:35:25 +0200

    change contentful

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,8 +1,12 @@
-import * as contentful from "contentful";
+import { createClient } from "contentful";
+import contentful from 'contentful'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
-  const client = contentful.createClient({
+  const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
+
+
+  const client = createClientFunc({
     space: config.public.contentfulSpace,
     accessToken: config.public.contentfulPublicAccessToken,
   });
